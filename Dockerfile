d3cb65c67450afc8335744d3324fb0a2ab7ee144
Alex Marston <alexander.marston@gmail.com>
2019-08-08 19:28:39 +0100

    Update Dockerfile and update copyright year

--- Dockerfile
+++ Dockerfile
@@ -1,5 +1,7 @@
 FROM php:7.0-apache
 
-RUN mkdir -p /var/www/html/vnstat/
-ADD . /var/www/html/vnstat/
+MAINTAINER Alex Marston <alexander.marston@gmail.com>
+
+COPY ./app/ /var/www/html/
+
 RUN mkdir -p /var/lib/vnstat
--- app/index.php
+++ app/index.php
@@ -1,6 +1,6 @@
 <?php
 /*
- * Copyright (C) 2016 Alexander Marston (alexander.marston@gmail.com)
+ * Copyright (C) 2019 Alexander Marston (alexander.marston@gmail.com)
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
