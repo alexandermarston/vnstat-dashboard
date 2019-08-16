5a7325d23947f00939a3571164e1738d4e19291c
Alex Marston <alexander.marston@gmail.com>
2019-08-16 09:55:45 +0000

    Add some incomplete test functions

--- tests/vnstatTest.php
+++ tests/vnstatTest.php
@@ -6,13 +6,60 @@ use PHPUnit\Framework\TestCase;
 
 final class vnStatTest extends TestCase {
 
+    private $vnStat;
+
     /**
     * Class constructor
     */
     public function testClassCanBeCreatedFromValidExecutablePath(): void {
-        $object = new vnStat('/usr/bin/vnstat');
+        $this->vnStat = new vnStat('/usr/bin/vnstat');
 
-        $this->assertInstanceOf('alexandermarston\vnStat', $object, 'Class Constructor');
+        $this->assertInstanceOf('alexandermarston\vnStat', $this->vnStat, 'Class Constructor');
+    }
+
+    /**
+     * @covers \alexandermarston\vnStat::processVnstatData
+     */
+    public function testProcessVnstatData(): void
+    {
+        /** @todo Complete this unit test method. */
+        $this->markTestIncomplete();
+    }
+
+    /**
+     * @covers \alexandermarston\vnStat::getVnstatVersion
+     */
+    public function testGetVnstatVersion(): void
+    {
+        /** @todo Complete this unit test method. */
+        $this->markTestIncomplete();
+    }
+
+    /**
+     * @covers \alexandermarston\vnStat::getVnstatJsonVersion
+     */
+    public function testGetVnstatJsonVersion(): void
+    {
+        /** @todo Complete this unit test method. */
+        $this->markTestIncomplete();
+    }
+
+    /**
+     * @covers \alexandermarston\vnStat::getInterfaces
+     */
+    public function testGetInterfaces(): void
+    {
+        /** @todo Complete this unit test method. */
+        $this->markTestIncomplete();
+    }
+
+    /**
+     * @covers \alexandermarston\vnStat::getInterfaceData
+     */
+    public function testGetInterfaceData(): void
+    {
+        /** @todo Complete this unit test method. */
+        $this->markTestIncomplete();
     }
 
 }
